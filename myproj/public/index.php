6910bed3a0af7bdd4b91036887cd85bea80f32ad
frombegin <frombegin@gmail.com>
2014-06-29 22:28:27 +0800

    add some comments.

--- myproj/public/index.php
+++ myproj/public/index.php
@@ -4,9 +4,10 @@
  * copyright (c) 2014, frombegin at gmail.com.
  */
 
+// register autoloaders
 require_once dirname(__DIR__) . '/frameworks/My/Autoloader.php';
-
 My\Autoloader::register();
 
+// create application & run
 $app = new My\Application();
 $app->run();
