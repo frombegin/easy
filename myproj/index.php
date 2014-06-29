39ec21d1b613656b067976db9965344e75a85996
frombegin <frombegin@gmail.com>
2014-06-29 21:52:31 +0800

    create nb project.

---
+++ myproj/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<!--
+copyright (c) 2014, frombegin at gmail.com.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
