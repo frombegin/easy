9811b4fefd727b69ce794dfecbe63acdf7062c08
frombegin <frombegin@gmail.com>
2014-06-29 21:59:45 +0800

    add Autoloader, Application class.

---
+++ myproj/frameworks/My/Application.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+ * copyright (c) 2014, frombegin at gmail.com.
+ */
+
+namespace My;
+
+/**
+ * Description of Application
+ *
+ * @author baohua
+ */
+class Application {
+    //put your code here
+}
---
+++ myproj/frameworks/My/Autoloader.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+ * copyright (c) 2014, frombegin at gmail.com.
+ */
+
+namespace My;
+
+/**
+ * Description of Autoloader
+ *
+ * @author baohua
+ */
+class Autoloader {
+    //put your code here
+}
