27a11bc4c9ce1617d705bd11d3227f1ede22d3fe
frombegin <frombegin@gmail.com>
2014-06-30 00:06:39 +0800

    add BadConfiguationException.

---
+++ myproj/frameworks/My/BadConfigurationException.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+ * copyright (c) 2014, frombegin at gmail.com.
+ */
+
+namespace My;
+
+/**
+ * Description of BadConfigurationException
+ *
+ * @author baohua
+ */
+class BadConfigurationException extends Exception {
+    //put your code here
+}
--- myproj/frameworks/My/Registry.php
+++ myproj/frameworks/My/Registry.php
@@ -13,10 +13,22 @@ namespace My;
  */
 class Registry {
 
+    /**
+     * get value of key
+     * 
+     * @param string $key
+     * @return mixed|null return value of key
+     */
     public function get($key) {
         
     }
     
+    /**
+     * set value of key
+     * 
+     * @param string $key
+     * @param mixed $value
+     */
     public function set($key, $value) {
         
     }
